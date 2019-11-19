c124aee78d160f8c063c03d2e2fae8f45d09c683
Sander van Dinteren <sanvandinteren@gmail.com>
2019-11-19 15:34:51 +0100

    Initial

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <p>aboutjes</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'about',
+  components: {
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <chatbar></chatbar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Chatbar from '../components/Chatbar.vue';
+
+export default {
+  name: "home",
+  components: {
+    Chatbar
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
